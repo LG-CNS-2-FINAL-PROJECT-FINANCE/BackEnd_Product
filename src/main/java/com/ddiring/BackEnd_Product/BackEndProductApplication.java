89e4f58d6eceb2e23641788f1ecb4f6dbf28e7a9
lyg0527 <lyg0527@naver.com>
2025-07-25 14:22:27 +0900

    Initial commit

---
+++ src/main/java/com/ddiring/BackEnd_Product/BackEndProductApplication.java
@@ -0,0 +1,13 @@
+package com.ddiring.BackEnd_Product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackEndProductApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackEndProductApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ddiring/BackEnd_Product/BackEndProductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ddiring.BackEnd_Product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackEndProductApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
