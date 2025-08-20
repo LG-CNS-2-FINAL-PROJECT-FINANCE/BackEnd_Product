9104bfefae974afb2c6e84218eed667fe2647e39
lyg0527 <lyg0527@naver.com>
2025-08-20 13:31:26 +0900

    test21

--- src/main/java/com/ddiring/BackEnd_Product/dto/creator/CreatorStopDto.java
+++ src/main/java/com/ddiring/BackEnd_Product/dto/creator/CreatorStopDto.java
@@ -1,6 +1,7 @@
 package com.ddiring.BackEnd_Product.dto.creator;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -17,7 +18,7 @@ public class CreatorStopDto {
     @NotBlank(message="대상 프로젝트 ID 필요")
     private String projectId;
 
-    @NotBlank(message = "파일을 등록하세요")
+    @NotNull(message = "파일을 등록하세요")
     private List<String> document;
 
     @Size(max = 500)
--- src/main/java/com/ddiring/BackEnd_Product/dto/creator/CreatorUpdateDto.java
+++ src/main/java/com/ddiring/BackEnd_Product/dto/creator/CreatorUpdateDto.java
@@ -1,6 +1,7 @@
 package com.ddiring.BackEnd_Product.dto.creator;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -30,7 +31,7 @@ public class CreatorUpdateDto {
     private BigDecimal goalAmount;
     private BigDecimal minInvestment;
 
-    @NotBlank(message = "파일을 등록하세요")
+    @NotNull(message = "파일을 등록하세요")
     private List<String> document;
 
     @Size(max = 500)
