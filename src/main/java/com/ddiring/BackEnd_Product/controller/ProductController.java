1bab65cfa9ec378507c1bfd2b4bfc229e1bf2aee
lyg0527 <lyg0527@naver.com>
2025-08-27 14:43:38 +0900

    complete17

--- src/main/java/com/ddiring/BackEnd_Product/controller/ProductController.java
+++ src/main/java/com/ddiring/BackEnd_Product/controller/ProductController.java
@@ -1,5 +1,6 @@
 package com.ddiring.BackEnd_Product.controller;
 
+import com.ddiring.BackEnd_Product.common.exception.CustomException;
 import com.ddiring.BackEnd_Product.common.security.JwtAuthGuard;
 import com.ddiring.BackEnd_Product.common.util.GatewayRequestHeaderUtils;
 import com.ddiring.BackEnd_Product.dto.product.ProductDetailDto;
@@ -27,7 +28,12 @@ public class ProductController {
 
     @GetMapping("/{projectId}")
     public ResponseEntity<ProductDetailDto> getProduct(@PathVariable String projectId) {
-        String userSeq = GatewayRequestHeaderUtils.getUserSeq();
+        String userSeq = null;
+        try {
+            userSeq = GatewayRequestHeaderUtils.getUserSeq();
+        } catch (CustomException ex) {
+            // 헤더 없으면 그냥 null로 둠 (비로그인 사용자)
+        }
         ProductDetailDto pdd = ps.getProductByProjectId(projectId, userSeq);
         return ResponseEntity.ok(pdd);
     }
