2f964dcac9c948bb84b73e8c2a6ba18a0b62553f
lyg0527 <lyg0527@naver.com>
2025-09-04 09:28:10 +0900

    kafkatest6

--- src/main/java/com/ddiring/BackEnd_Product/kafka/NotificationProducer.java
+++ src/main/java/com/ddiring/BackEnd_Product/kafka/NotificationProducer.java
@@ -1,5 +1,6 @@
 package com.ddiring.BackEnd_Product.kafka;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
@@ -12,26 +13,37 @@ import java.util.UUID;
 @RequiredArgsConstructor
 public class NotificationProducer {
 
-    private final KafkaTemplate<String, Object> kafkaTemplate;
+    private final KafkaTemplate<String, String> kafkaTemplate;
+    private final ObjectMapper objectMapper;
     private static final String TOPIC = "notification";
 
-    // 단일 대상
+    //단일 사용자 알림 전송 (단일도 List.of 처리)
     public void sendNotification(String userSeq, String notificationType, String title, String message) {
         sendNotification(List.of(userSeq), notificationType, title, message);
     }
 
-    // 다중 대상
+    //다중 사용자 알림 전송
     public void sendNotification(List<String> userSeq, String notificationType, String title, String message) {
-        NotificationEvent event = NotificationEvent.builder()
-                .eventId(UUID.randomUUID().toString())
-//                .timestamp(Instant.now())
-                .userSeq(userSeq)
-                .notificationType(notificationType)
-                .title(title)
-                .message(message)
-                .build();
+        try {
+            NotificationPayload payload = NotificationPayload.builder()
+                    .userSeq(userSeq)
+                    .notificationType(notificationType)
+                    .title(title)
+                    .message(message)
+                    .build();
 
-        kafkaTemplate.send(TOPIC, event);
-        System.out.println("Kafka 메시지 전송 완료: " + event);
+            EventEnvelope<NotificationPayload> envelope = EventEnvelope.<NotificationPayload>builder()
+                    .eventId(UUID.randomUUID().toString())
+                    .timestamp(Instant.now())
+                    .payload(payload)
+                    .build();
+
+            String json = objectMapper.writeValueAsString(envelope);
+            kafkaTemplate.send(TOPIC, json);
+
+            System.out.println("Kafka 메시지 전송 완료: " + json);
+        } catch (Exception e) {
+            throw new RuntimeException("Kafka 메시지 전송 실패", e);
+        }
     }
 }
